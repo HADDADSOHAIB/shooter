2ba3e0c4757594bf2af00212cd45a7bba30b404f
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-05-26 21:05:18 +0000

    fix resource issue

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(path.join(__dirname, '/')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
