d3510b30cb55b2d34a948e719a1938f0a47cf76f
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-05-26 20:34:57 +0000

    add server file

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.listen(port);
