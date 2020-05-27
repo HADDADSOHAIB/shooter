5eb8faf8b9dd73f89092e8f5f388caeef96d9476
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-05-27 17:59:40 +0000

    fix sound bug in options scene on restart

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -16,11 +16,11 @@ export default class TitleScene extends Phaser.Scene {
     this.creditsButton = new Button(this, config.width / 2, config.height / 2 + 100, 'blueButton1', 'blueButton2', 'Credits', 'Credits');
 
     this.model = this.sys.game.globals.model;
+    this.bgMusic = this.sound.add('bgMusic', { volume: 0.5, loop: true });
     if (this.model.musicOn === true && this.model.bgMusicPlaying === false) {
-      this.bgMusic = this.sound.add('bgMusic', { volume: 0.5, loop: true });
       this.bgMusic.play();
       this.model.bgMusicPlaying = true;
-      this.sys.game.globals.bgMusic = this.bgMusic;
     }
+    this.sys.game.globals.bgMusic = this.bgMusic;
   }
 }
